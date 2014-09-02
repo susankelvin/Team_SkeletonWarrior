661cb32efca81160db940db40c3eecc8aa5dd8fe
susankelvin <susankelvin@users.noreply.github.com>
2014-09-02 13:02:34 +0300

    MySQL Models added

---
+++ MobileVendors.Models/Store.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileVendors.Models
+{
+    public class Store
+    {
+        private ICollection<Subscription> subscriptions;
+
+        public Store()
+        {
+            this.subscriptions = new HashSet<Subscription>();
+        }
+
+        public int Id { get; set; }
+
+        public string Address { get; set; }
+
+        public int VendorId { get; set; }
+
+        public virtual Vendor Vendor { get; set; }
+
+        public int TownId { get; set; }
+
+        public virtual Town Town { get; set; }
+
+        public virtual ICollection<Subscription> Subscriptions
+        {
+            get
+            {
+                return this.subscriptions;
+            }
+            set
+            {
+                this.subscriptions = value;
+            }
+        }
+    }
+}
---
+++ MobileVendors.Models/Town.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileVendors.Models
+{
+    public class Town
+    {
+        private ICollection<Store> stores;
+
+        public Town()
+        {
+            this.stores = new HashSet<Store>();
+        }
+
+        public int Id { get; set; }
+
+        public string TownName { get; set; }
+
+        public virtual ICollection<Store> Stores
+        {
+            get
+            {
+                return this.stores;
+            }
+            set
+            {
+                this.stores = value;
+            }
+        }
+    }
+}
